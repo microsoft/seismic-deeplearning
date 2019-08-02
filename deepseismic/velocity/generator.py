b8e71f9ea28bf2a7b08dfb1d7f70c94658d3f4f5
Gianluca Campanella <gianluca.campanella@microsoft.com>
2019-08-02 09:59:35 +0000

    Merged PR 52: PRNG seed
    Exposed PRNG seed in generative models for velocities

--- deepseismic/velocity/generator.py
+++ deepseismic/velocity/generator.py
@@ -5,10 +5,14 @@ import numpy as np
 
 class Generator(object):
     def __init__(
-        self, shape: Tuple[int, ...], dtype: Optional[type] = np.float32
+        self,
+        shape: Tuple[int, ...],
+        dtype: Optional[type] = np.float32,
+        seed: Optional[int] = None,
     ):
         self.shape = shape
         self.dtype = dtype
+        self._prng = np.random.RandomState(seed)
 
     def generate(self, seed: Optional[int]) -> np.ndarray:
         raise NotImplementedError
--- deepseismic/velocity/roeth_tarantola.py
+++ deepseismic/velocity/roeth_tarantola.py
@@ -10,12 +10,13 @@ class RoethTarantolaGenerator(Generator):
         self,
         shape: Tuple[int, ...],
         dtype: Optional[type] = np.float32,
+        seed: Optional[int] = None,
         horizontal_dim: Optional[int] = -1,
         nlayers: Optional[int] = 8,
         initial_vp: Optional[Tuple[float, float]] = (1350.0, 1650.0),
         vp_perturbation: Optional[Tuple[float, float]] = (-190.0, 570.0),
     ):
-        super().__init__(shape, dtype)
+        super().__init__(shape, dtype, seed)
         self.horizontal_dim = horizontal_dim
         self.nlayers = nlayers
         self.initial_vp = initial_vp
@@ -32,9 +33,9 @@ class RoethTarantolaGenerator(Generator):
         for i in range(self.nlayers):
             vp_idx[dim] = slice(layer_idx[i], layer_idx[i + 1])
             layer_vp = (
-                np.random.uniform(*self.initial_vp)
+                self._prng.uniform(*self.initial_vp)
                 if layer_vp is None
-                else layer_vp + np.random.uniform(*self.vp_perturbation)
+                else layer_vp + self._prng.uniform(*self.vp_perturbation)
             )
             vp[tuple(vp_idx)] = layer_vp
         return vp
