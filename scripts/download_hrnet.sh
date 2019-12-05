1111c6db0fbb94fddd2d847061045a18a8c4182d
Mat <msalvaris@users.noreply.github.com>
2019-12-05 16:30:22 +0000

    Fixes download script  (#84)
    * Fixes download script
    * Updates readme

--- scripts/download_hrnet.sh
+++ scripts/download_hrnet.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
+#
+# Example:
+# download_hrnet.sh /data/models hrnet.pth
+#
 
 echo Using "$1" as the download directory
 
@@ -14,6 +18,10 @@ full_path=$1/$2
 
 echo "Downloading to ${full_path}"
 
-wget \
-'https://optgaw.dm.files.1drv.com/y4mYRZ1J12ATHBWxSwZJTycIFbgT88SoWegSN3NXEOJFTJWBLDZ3nyCbj-sDvmGobL2CAGzhPobs5gMt3466nKbATNs9toc5N569Z5xNicNUABQm0MVucO7Vi7cjP__n2MFL5qDZyL4cOx6VgoNjpb9lglVRqoTVfVHdJ3sM7qO-9sAODNzgmKrCrU7uHvB54YtsKLr51Qi6BlDn94DalmEJQ/hrnetv2_w48_imagenet_pretrained.pth?download&psid=1' \
---output-document "${full_path}"
+wget --header 'Host: optgaw.dm.files.1drv.com' \
+      --user-agent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0' \
+      --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' \
+      --header 'Accept-Language: en-GB,en;q=0.5' \
+      --referer 'https://onedrive.live.com/' \
+      --header 'Upgrade-Insecure-Requests: 1' 'https://optgaw.dm.files.1drv.com/y4m14W1OEuoniQMCT4m64UV8CSQT-dFe2ZRhU0LAZSal80V4phgVIlTYxI2tUi6BPVOy7l5rK8MKpZNywVvtz-NKL2ZWq-UYRL6MAjbLgdFA6zyW8RRrKBe_FcqcWr4YTXeJ18xfVqco6CdGZHFfORBE6EtFxEIrHWNjM032dWZLdqZ0eXd7RZTrHs1KKYa92zcs0Rj91CAyIK4hIaOomzEWA/hrnetv2_w48_imagenet_pretrained.pth?download&psid=1' \
+      --output-document ${full_path}
