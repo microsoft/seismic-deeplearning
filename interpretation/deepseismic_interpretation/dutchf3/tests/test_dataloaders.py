1e4ee640fcda5bb1a902c27ab264d337e16b1389
Max Kaznady <maxkaz@microsoft.com>
2020-06-10 16:58:28 +0000

    fixed dataloader tests - updated them to work with new code from staging branch

--- interpretation/deepseismic_interpretation/dutchf3/tests/test_dataloaders.py
+++ interpretation/deepseismic_interpretation/dutchf3/tests/test_dataloaders.py
@@ -6,7 +6,11 @@ Tests for TrainLoader and TestLoader classes when overriding the file names of t
 
 import tempfile
 import numpy as np
-from interpretation.deepseismic_interpretation.dutchf3.data import get_test_loader, TrainPatchLoaderWithDepth, TrainSectionLoaderWithDepth
+from interpretation.deepseismic_interpretation.dutchf3.data import (
+    get_test_loader,
+    TrainPatchLoaderWithDepth,
+    TrainSectionLoaderWithDepth,
+)
 import pytest
 import yacs.config
 import os
@@ -15,11 +19,12 @@ import os
 IL = 5
 XL = 10
 D = 8
+N_CLASSES = 2
 
-CONFIG_FILE = "./examples/interpretation/notebooks/configs/unet.yaml"
+CONFIG_FILE = "./experiments/interpretation/dutchf3_patch/local/configs/unet.yaml"
+
 with open(CONFIG_FILE, "rt") as f_read:
     config = yacs.config.load_cfg(f_read)
-
 
 def generate_npy_files(path, data):
     np.save(path, data)
@@ -52,10 +57,11 @@ def test_TestSectionLoader_should_load_data_from_test1_set():
         generate_npy_files(os.path.join(data_dir, "test_once", "test1_labels.npy"), labels)
 
         txt_path = os.path.join(data_dir, "splits", "section_test1.txt")
-        open(txt_path, 'a').close()
+        open(txt_path, "a").close()
 
         TestSectionLoader = get_test_loader(config)
-        test_set = TestSectionLoader(data_dir = data_dir, split = 'test1')
+        config.merge_from_list(['DATASET.ROOT', data_dir])
+        test_set = TestSectionLoader(config, split="test1")
 
         assert_dimensions(test_set)
 
@@ -74,10 +80,11 @@ def test_TestSectionLoader_should_load_data_from_test2_set():
         generate_npy_files(os.path.join(data_dir, "test_once", "test2_labels.npy"), labels)
 
         txt_path = os.path.join(data_dir, "splits", "section_test2.txt")
-        open(txt_path, 'a').close()
+        open(txt_path, "a").close()
 
         TestSectionLoader = get_test_loader(config)
-        test_set = TestSectionLoader(data_dir = data_dir, split = 'test2')
+        config.merge_from_list(['DATASET.ROOT', data_dir])
+        test_set = TestSectionLoader(config, split="test2")
 
         assert_dimensions(test_set)
 
@@ -94,143 +101,20 @@ def test_TestSectionLoader_should_load_data_from_path_override_data():
         generate_npy_files(os.path.join(data_dir, "volume_name", "labels.npy"), labels)
 
         txt_path = os.path.join(data_dir, "splits", "section_volume_name.txt")
-        open(txt_path, 'a').close()
+        open(txt_path, "a").close()
 
         TestSectionLoader = get_test_loader(config)
-        test_set = TestSectionLoader(data_dir = data_dir,
-                                     split = "volume_name",
-                                     is_transform = True,
-                                     augmentations = None,
-                                     seismic_path = os.path.join(data_dir, "volume_name", "seismic.npy"),
-                                     label_path = os.path.join(data_dir, "volume_name", "labels.npy"))
+        config.merge_from_list(['DATASET.ROOT', data_dir])
+        test_set = TestSectionLoader(
+            config,
+            split="volume_name",
+            is_transform=True,
+            augmentations=None,
+            seismic_path=os.path.join(data_dir, "volume_name", "seismic.npy"),
+            label_path=os.path.join(data_dir, "volume_name", "labels.npy")
+        )
 
         assert_dimensions(test_set)
-
-def test_TrainSectionLoaderWithDepth_should_fail_on_empty_file_names(tmpdir):
-    """
-    Check for exception when files do not exist
-    """
-
-    # Test
-    with pytest.raises(Exception) as excinfo:
-
-        _ = TrainSectionLoaderWithDepth(
-            data_dir = tmpdir,
-            split = "volume_name",
-            is_transform=True,
-            augmentations=None,
-            seismic_path = "",
-            label_path = ""
-        )
-    assert "does not exist" in str(excinfo.value)
-
-
-def test_TrainSectionLoaderWithDepth_should_fail_on_missing_seismic_file(tmpdir):
-    """
-    Check for exception when training param is empty
-    """
-    # Setup
-    os.makedirs(os.path.join(tmpdir, "volume_name"))
-    os.makedirs(os.path.join(tmpdir, "splits"))
-
-    labels = np.ones([IL, XL, D])
-    generate_npy_files(os.path.join(tmpdir, "volume_name", "labels.npy"), labels)
-
-    txt_path = os.path.join(tmpdir, "splits", "patch_volume_name.txt")
-    open(txt_path, 'a').close()
-
-    # Test
-    with pytest.raises(Exception) as excinfo:
-
-        _ = TrainSectionLoaderWithDepth(
-            data_dir = tmpdir,
-            split = "volume_name",
-            is_transform=True,
-            augmentations=None,
-            seismic_path=os.path.join(tmpdir, "volume_name", "seismic.npy"),
-            label_path=os.path.join(tmpdir, "volume_name", "labels.npy")
-        )
-    assert "does not exist" in str(excinfo.value)
-
-
-def test_TrainSectionLoaderWithDepth_should_fail_on_missing_label_file(tmpdir):
-    """
-    Check for exception when training param is empty
-    """
-    # Setup
-    os.makedirs(os.path.join(tmpdir, "volume_name"))
-    os.makedirs(os.path.join(tmpdir, "splits"))
-
-    labels = np.ones([IL, XL, D])
-    generate_npy_files(os.path.join(tmpdir, "volume_name", "labels.npy"), labels)
-
-    txt_path = os.path.join(tmpdir, "splits", "patch_volume_name.txt")
-    open(txt_path, 'a').close()
-
-    # Test
-    with pytest.raises(Exception) as excinfo:
-
-        _ = TrainSectionLoaderWithDepth(
-            data_dir = tmpdir,
-            split = "volume_name",
-            is_transform=True,
-            augmentations=None,
-            seismic_path=os.path.join(tmpdir, "volume_name", "seismic.npy"),
-            label_path=os.path.join(tmpdir, "volume_name", "labels.npy")
-        )
-    assert "does not exist" in str(excinfo.value)
-
-
-def test_TrainSectionLoaderWithDepth_should_load_with_one_train_and_label_file(tmpdir):
-    """
-    Check for successful class instantiation w/ single npy file for train & label
-    """
-    # Setup
-    os.makedirs(os.path.join(tmpdir, "volume_name"))
-    os.makedirs(os.path.join(tmpdir, "splits"))
-
-    seimic = np.zeros([IL, XL, D])
-    generate_npy_files(os.path.join(tmpdir, "volume_name", "seismic.npy"), seimic)
-
-    labels = np.ones([IL, XL, D])
-    generate_npy_files(os.path.join(tmpdir, "volume_name", "labels.npy"), labels)
-
-    txt_path = os.path.join(tmpdir, "splits", "section_volume_name.txt")
-    open(txt_path, 'a').close()
-
-    # Test
-    train_set = TrainSectionLoaderWithDepth(
-        data_dir = tmpdir,
-        split = "volume_name",
-        is_transform=True,
-        augmentations=None,
-        seismic_path=os.path.join(tmpdir, "volume_name", "seismic.npy"),
-        label_path=os.path.join(tmpdir, "volume_name", "labels.npy")
-    )
-
-    assert train_set.labels.shape == (IL, XL, D)
-    assert train_set.seismic.shape == (IL, 3, XL, D)
-
-
-def test_TrainPatchLoaderWithDepth_should_fail_on_empty_file_names(tmpdir):
-    """
-    Check for exception when files do not exist
-    """
-    # Test
-    with pytest.raises(Exception) as excinfo:
-
-        _ = TrainPatchLoaderWithDepth(
-            data_dir = tmpdir,
-            split = "volume_name",
-            is_transform=True,
-            stride=25,
-            patch_size=100,
-            augmentations=None,
-            seismic_path = "",
-            label_path = ""
-        )
-    assert "does not exist" in str(excinfo.value)
-
 
 def test_TrainPatchLoaderWithDepth_should_fail_on_missing_seismic_file(tmpdir):
     """
@@ -244,20 +128,20 @@ def test_TrainPatchLoaderWithDepth_should_fail_on_missing_seismic_file(tmpdir):
     generate_npy_files(os.path.join(tmpdir, "volume_name", "labels.npy"), labels)
 
     txt_path = os.path.join(tmpdir, "splits", "patch_volume_name.txt")
-    open(txt_path, 'a').close()
+    open(txt_path, "a").close()
+
+    config.merge_from_list(['DATASET.ROOT', str(tmpdir)])
 
     # Test
     with pytest.raises(Exception) as excinfo:
 
         _ = TrainPatchLoaderWithDepth(
-            data_dir = tmpdir,
-            split = "volume_name",
+            config,
+            split="volume_name",
             is_transform=True,
-            stride=25,
-            patch_size=100,
             augmentations=None,
             seismic_path=os.path.join(tmpdir, "volume_name", "seismic.npy"),
-            label_path=os.path.join(tmpdir, "volume_name", "labels.npy")
+            label_path=os.path.join(tmpdir, "volume_name", "labels.npy"),
         )
     assert "does not exist" in str(excinfo.value)
 
@@ -274,17 +158,17 @@ def test_TrainPatchLoaderWithDepth_should_fail_on_missing_label_file(tmpdir):
     generate_npy_files(os.path.join(tmpdir, "volume_name", "seismic.npy"), seimic)
 
     txt_path = os.path.join(tmpdir, "splits", "patch_volume_name.txt")
-    open(txt_path, 'a').close()
+    open(txt_path, "a").close()
+
+    config.merge_from_list(['DATASET.ROOT', str(tmpdir)])
 
     # Test
     with pytest.raises(Exception) as excinfo:
 
         _ = TrainPatchLoaderWithDepth(
-            data_dir = tmpdir,
-            split = "volume_name",
+            config,
+            split="volume_name",
             is_transform=True,
-            stride=25,
-            patch_size=100,
             augmentations=None,
             seismic_path=os.path.join(tmpdir, "volume_name", "seismic.npy"),
             label_path=os.path.join(tmpdir, "volume_name", "labels.npy")
@@ -306,16 +190,17 @@ def test_TrainPatchLoaderWithDepth_should_load_with_one_train_and_label_file(tmp
     labels = np.ones([IL, XL, D])
     generate_npy_files(os.path.join(tmpdir, "volume_name", "labels.npy"), labels)
 
-    txt_path = os.path.join(tmpdir, "splits", "patch_volume_name.txt")
-    open(txt_path, 'a').close()
+    txt_dir = os.path.join(tmpdir, "splits")
+    txt_path = os.path.join(txt_dir, "patch_volume_name.txt")
+    open(txt_path, "a").close()
+
+    config.merge_from_list(['DATASET.ROOT', str(tmpdir)])
 
     # Test
     train_set = TrainPatchLoaderWithDepth(
-        data_dir = tmpdir,
-        split = "volume_name",
+        config,
+        split="volume_name",
         is_transform=True,
-        stride=25,
-        patch_size=100,
         augmentations=None,
         seismic_path=os.path.join(tmpdir, "volume_name", "seismic.npy"),
         label_path=os.path.join(tmpdir, "volume_name", "labels.npy")
